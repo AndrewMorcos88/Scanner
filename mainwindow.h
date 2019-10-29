188291d7ae1b6b357f7bc54936cc8dd004bd67e5
andrew_morcos <36261350+Andro-Morcos-Nagib@users.noreply.github.com>
2019-10-29 19:58:59 +0200

    First Init

---
+++ mainwindow.h
@@ -0,0 +1,93 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTableWidget>
+#include<qiterator.h>
+#include <QString>
+#include<QMap>
+#include <QPalette>
+#include <QGraphicsScene>
+#include <QFormLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QComboBox>
+#include <QLineEdit>
+#include <QLabel>
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
+#include <QVector>
+#include <QList>
+#include <QPair>
+#include<QMessageBox>
+#include <QChar>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    QVector<QString>TOKENS_TYPE;
+    QVector<QString>TOKENS_VALUE;
+    QMap<QString, int>Reserved_Words;
+    QMap<QString, int>::iterator it;
+    QMap<QString, int>IDENTIFIERS;
+    QMap<QString, int>Operators_Assigments;
+    void InitUI();
+      void InitFisrtPage();
+      QVector<QString>TEXT;
+
+
+      void Check_Reserved_Keywords(int target, QVector<QString>& TOKENS_TYPE, QVector<QString> &TOKENS_VALUE,
+          bool &Wait_FOR_IDENTIFIERS , bool & Wait_FOR_SemiCOLumn);
+      void Check_Operators(int target, QVector<QString>& TOKENS_TYPE, QVector<QString> &TOKENS_VALUE
+          , bool & Wait_FOR_SemiCOLumn, bool &Wait_FOR_IDENTIFIERS);
+
+
+
+
+
+
+
+public slots:
+    void browseButtonPressed();
+    void runAlgorithmPressed();
+
+
+
+private:
+    Ui::MainWindow *ui;
+    QWidget *centralWidget;
+    QWidget *AnotherWidget;
+    QGraphicsScene *scene;
+    //colors and pallete
+    QPalette *mainPalette;
+    //layout
+    QFormLayout *firstInputPage;
+    QHBoxLayout *mainLayout;
+    QHBoxLayout *inputLayout;
+    QHBoxLayout *graphicsLayout;
+
+    // buttons
+    QPushButton *browseButton;
+    QPushButton *RunButton;
+    //comboBox
+    QComboBox *algorithm;
+    //label
+    QLabel *chooseAlgorithm;
+    //text
+    QLineEdit *fileName;
+    QLineEdit * First_Line;
+    QLineEdit * Second_Line;
+
+   QTableWidget * My_Table;
+
+};
+
+#endif // MAINWINDOW_H
